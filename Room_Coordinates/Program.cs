992d47919915a987f476e3e760d00af7bb13891e
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-20 17:59:56 -0500

    Add project files.

---
+++ Room_Coordinates/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Build test Coordinate Objects
+Coordinate Check1 = new Coordinate(1, 2);
+Coordinate Check2 = new Coordinate(2, 3);
+Coordinate Check3 = new Coordinate(3,3);
+Coordinate Check4 = new Coordinate(9, 8);
+
+Console.WriteLine(Coordinate.AdjacentCheck(Check1, Check2));
+
+
+
+public struct Coordinate
+{
+    public int Row { get; }
+    public int Column { get; }
+
+    public Coordinate(int ProvidedRow, int ProvidedColumn)
+    {
+        Row = ProvidedRow;
+        Column = ProvidedColumn;
+    }
+
+    public static bool AdjacentCheck(Coordinate First, Coordinate Second)
+    {
+        int SetRow = First.Row - Second.Row;
+        int CheckRow = CheckNegative(SetRow);
+        int SetColumn = First.Column - Second.Column;
+        int CheckColumn = CheckNegative(SetColumn);
+    }
+
+    private static int CheckNegative(int SetCheck);
+    {
+        if (SetCheck < 0) 
+    }
+}
+
+
