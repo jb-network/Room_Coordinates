36e5628a48eff33546315008307165d661616540
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-20 19:04:35 -0500

    Update Program.cs

--- Room_Coordinates/Program.cs
+++ Room_Coordinates/Program.cs
@@ -9,26 +9,31 @@ Coordinate Check3 = new Coordinate("Check 3", 4, 5);
 Coordinate Check4 = new Coordinate("Check 4", 9, 8);
 
 //Output for the program
+
 //Test 1
 Console.WriteLine($"Checking Adjacency for: {Check1.Name} and {Check2.Name}");
 Console.WriteLine($"{Check1.Name} is set to ({Check1.Column},{Check1.Row})");
 Console.WriteLine($"{Check2.Name} is set to ({Check2.Column},{Check2.Row})");
 Console.WriteLine($"They are Adjacent: {Coordinate.AdjacentCheck(Check1, Check2)} \n");
+
 //Test 2
 Console.WriteLine($"Checking Adjacency for: {Check1.Name} and {Check3.Name}");
 Console.WriteLine($"{Check1.Name} is set to ({Check1.Column},{Check1.Row})");
 Console.WriteLine($"{Check3.Name} is set to ({Check3.Column},{Check3.Row})");
 Console.WriteLine($"They are Adjacent: {Coordinate.AdjacentCheck(Check1, Check3)} \n");
+
 //Test 3
 Console.WriteLine($"Checking Adjacency for: {Check2.Name} and {Check3.Name}");
 Console.WriteLine($"{Check2.Name} is set to ({Check2.Column},{Check2.Row})");
 Console.WriteLine($"{Check3.Name} is set to ({Check3.Column},{Check3.Row})");
 Console.WriteLine($"They are Adjacent: {Coordinate.AdjacentCheck(Check2, Check3)} \n");
+
 //Test 4
 Console.WriteLine($"Checking Adjacency for: {Check1.Name} and {Check4.Name}");
 Console.WriteLine($"{Check1.Name} is set to ({Check1.Column},{Check1.Row})");
 Console.WriteLine($"{Check4.Name} is set to ({Check4.Column},{Check4.Row})");
 Console.WriteLine($"They are Adjacent: {Coordinate.AdjacentCheck(Check1, Check4)} \n");
+
 //Close
 Console.WriteLine("Press any key to end the test");
 Console.ReadKey();
@@ -47,7 +52,8 @@ public struct Coordinate
         Column = ProvidedColumn;
         
     }
-  //Pass output to check/convert possible negative number produced by the expression
+    
+    //Pass output to check/convert possible negative number produced by the expression
     public static bool AdjacentCheck(Coordinate First, Coordinate Second)
     {
       
@@ -59,7 +65,8 @@ public struct Coordinate
         else if (SetRow <= 1 && SetColumn == 0) return true;
         else return false;
     }
-//If SetCheck is a negative number return positive  of the number
+    
+    //If SetCheck is a negative number return positive of the number
     private static int CheckNegative(int SetCheck)  
     {   
         if (SetCheck < 0)
